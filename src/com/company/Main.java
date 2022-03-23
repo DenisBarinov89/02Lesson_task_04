f908e619c983e01e21e0de60fc4285443b593568
Denis Barinov <seocoup@yandex.ru>
2022-03-23 21:48:31 +0300

    Условные операторы - Задание 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int deliveryDistance = 95;
+        int timeToClient = 0;
+        int buffer = (deliveryDistance - 20) / 40;
+
+        if (deliveryDistance <= 20) {
+            timeToClient = 1;            
+        }
+        else {
+            timeToClient = buffer + 2;
+        }
+        System.out.println("Срок доставки:"+timeToClient);
     }
 }
